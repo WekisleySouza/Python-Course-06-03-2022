b92e1d2fdf4b268838bd89006d56dbe43c45800e
Wekisley Souza <wekisleysouza.a@gmail.com>
2022-03-10 11:19:24 -0300

    Substituição do while no validador de CNPJ devido a bug.

--- Validar CNPJ/main.py
+++ Validar CNPJ/main.py
@@ -1,15 +1,13 @@
 from Functions import finalCNPJ
 
-tam = 0
-while tam == 0:
-    CNPJ = input("Qual é o seu cnpj [Somente os números]?")
-    print(len(str(CNPJ)))
-    if len(str(CNPJ)) == 12:
-        tam == 1
+CNPJ = input("Qual é o seu cnpj [Somente os números]?")
+
+if len(str(CNPJ)) != 12 or not CNPJ.isnumeric():
+    option = input("\nCNPJ inválido!\n")
+else:
+    validadoCNPJ = finalCNPJ(CNPJ[0, 10])
+
+    if validadoCNPJ == CNPJ:
+        print("CNPJ é válido!")
     else:
-        print("\nCNPJ inválido, tente novamente!\n")
-
-validadoCNPJ = "04252011000110"
-
-print(finalCNPJ(validadoCNPJ))
-
+        print("CNPJ não é válido!")
